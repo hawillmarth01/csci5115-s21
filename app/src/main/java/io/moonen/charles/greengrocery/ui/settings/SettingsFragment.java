4b12750d8c32917ad0c3f11da258a5c48c03b2c7
Charles Moonen <charles@moonen.io>
2021-03-25 14:57:34 -0500

    nav

---
+++ app/src/main/java/io/moonen/charles/greengrocery/ui/settings/SettingsFragment.java
@@ -0,0 +1,17 @@
+package io.moonen.charles.greengrocery.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import io.moonen.charles.greengrocery.R;
+
+public class SettingsFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
