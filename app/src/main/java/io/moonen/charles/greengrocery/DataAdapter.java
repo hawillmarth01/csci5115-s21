0c35516edbf3ce5fa88f5cc46c89898d90d8220a
Hannah Willmarth <willm087@umn.edu>
2021-03-24 21:58:20 -0500

    receipt/product scorecards, ex. receipt content management, csv file processing

---
+++ app/src/main/java/io/moonen/charles/greengrocery/DataAdapter.java
@@ -0,0 +1,48 @@
+package io.moonen.charles.greengrocery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Class used to convert row of data to a Product object
+public class DataAdapter {
+    private String[] currRow;
+
+    public DataAdapter(String[] incomingRow){
+        this.currRow = incomingRow;
+    }
+
+    public Product createProduct(){
+        //product and grade lists
+        List grades = new ArrayList<>();
+
+        //gor through given row
+        //row format:
+        //name, brand, location, location grade, in season, in season grade, emissions, emissions grade
+        //water consump, water consump grade, packaging, packaging grade, price, quality, overall sustain grade
+
+        //store product name and brand
+        String product_name = currRow[0];
+        String product_brand= currRow[1];
+
+
+        //create grade objects
+        String[] categories = {"Locality", "Seasonality", "Emissions","Water", "Packaging"};
+        int c = 0; //counter for categories
+        int endOfGrades = 11; //end of sustainability grades
+        for(int i = 2 ; i < endOfGrades; i+=2){
+            grades.add(new Grade(categories[c], currRow[i+1], currRow[i]));
+            c++; //increment category label
+        }
+        //add overall sustainability grade
+        grades.add(new Grade("Price", currRow[currRow.length-1], ""));
+
+        //store price and quality
+        String price = currRow[currRow.length-3];
+        String quality = currRow[currRow.length-2];
+
+        //create product
+        Product newProduct = new Product(product_name, product_brand, price, quality, grades);
+
+        return newProduct;
+    }
+}
---
+++ app/src/main/java/io/moonen/charles/greengrocery/ui/points_earned/PointsEarnedFragment.java
@@ -0,0 +1,38 @@
+package io.moonen.charles.greengrocery.ui.points_earned;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import io.moonen.charles.greengrocery.MainActivity;
+import io.moonen.charles.greengrocery.R;
+import io.moonen.charles.greengrocery.Receipt;
+
+//POINTS EARNED FRAGMENT
+public class PointsEarnedFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_points_earned, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(View v, Bundle savedInstanceState){
+        super.onViewCreated(v, savedInstanceState);
+        //get receipt data
+        MainActivity activity = (MainActivity) getActivity();
+        Receipt receipt = activity.getReceiptData();
+        String pts = receipt.getOverallPoints();
+
+        //set points value
+        TextView pointsEarned = v.findViewById(R.id.pointsEarned);
+        pointsEarned.setText("You Earned " + pts + " points!");
+    }
+}
